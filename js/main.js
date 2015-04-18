cfbe3256899db35f11a1f687eef7a20d99daa0b3
KikisAndres <kikisandres@gmail.com>
2015-04-18 01:19:54 -0600

    JavaScript Folder

--- js/main.js
+++ js/main.js
@@ -5,3 +5,35 @@ function validateForm () {
 				return false;
 		};
 } 
+
+
+var json_parse = function () {
+	var at,
+		ch,
+		escapee = {
+			'"': '"',
+			'\\': '\\',
+			'/': '/',
+			b: 'b',
+			f: '\f',
+			n: '\n',
+			r: '\r',
+			t: '\t',
+		},
+}
+
+//  <p id="demo"></p>
+ // Assign 5 to x
+
+x = 5
+
+elem = document.getElementById("demo"); // Find an element 
+elem.innerHTML = x;                     // Display x in the element
+
+var x; // Declare x
+
+var x = 5; // Initialize x
+var y = 7; // Initialize y
+
+elem = document.getElementById("demo"); // Find an element 
+elem.innerHTML = x + " " + y;           // Display x and y
